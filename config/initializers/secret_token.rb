99d2456c21aab017c9da17d4c7868982290b2440
Pete Vilter <pete.vilter@gmail.com>
2013-05-09 03:59:37 -0500

    rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BjscavItem214::Application.config.secret_token = 'b29a48ab3a5fa762cc2b7025e928d91af11e627206ffd2923750937f5a6c2373bddbed9169ed95e5bef227d8f43279e015ee3ebfb5133b2e2ed08f41421e4992'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BjscavItem214::Application.config.session_store :cookie_store, key: '_bjscav-item214_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BjscavItem214::Application.config.session_store :active_record_store
